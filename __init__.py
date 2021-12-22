3d931090c187923db459fa54016de36d6e5d7864
Pablo Herrero <pablodherrero@gmail.com>
2021-12-22 06:26:46 -0300

    Some fixes

--- __init__.py
+++ __init__.py
@@ -11,33 +11,33 @@ Synopsis: <trigger> [delay|throw] <query>"""
 
 from albert import *
 from pathlib import Path
+from unidecode import unidecode
 from urllib.parse import urlparse
 import os.path
 
 __title__ = "Mate Bookmarks"
 __version__ = "0.0.1"
 __authors__ = "Pablo Herrero"
+__py_deps__ = 'unidecode'
 
 iconPath = iconLookup("folder")
+xdg_config_dir = os.environ.get('XDG_CONFIG_HOME') or os.path.join(Path.home(), '.config')
 
-# Can be omitted
+def get_bookmark(url):
+    name = url.path.rstrip(os.sep).split(os.sep)[-1]
+    return (name, unidecode(name.lower()), f'Open {url.path if url.scheme == "file" else url.geturl()}', url.geturl())
+
 def initialize():
     global bookmarks
-    with open(os.path.join(Path.home(), '.config', 'gtk-3.0', 'bookmarks'), 'r') as file:
-        dirs = (urlparse(l) for l in file.read().splitlines())
-        entries = ((d.path.replace(f'{Path.home()}/', ''), d.path) for d in dirs if d.scheme == 'file' and d.path != Path.home())
-        bookmarks = [ (n, n.lower() , d) for n, d in entries ]
-
-# Can be omitted
-def finalize():
-    pass
+    with open(os.path.join(xdg_config_dir, 'gtk-3.0', 'bookmarks'), 'r') as file:
+        urls = (urlparse(l) for l in file.read().splitlines())
+        bookmarks = [get_bookmark(u) for u in urls if u.scheme != 'file' or u.path != Path.home()]
 
 def handleQuery(query):
-    results = []
+    if not query.string:
+        return
 
-    for name, exp, path in bookmarks:
-        if len(query.string) and exp.startswith(query.string.lower()):
-            item = Item(__title__, iconPath, name, f'Open {name} folder', [ProcAction("Open bookmark", ["xdg-open", path])])
-            results.append(item)
+    query = unidecode(query.string.lower())
 
-    return results
+    return [ Item(__title__, iconPath, name, text, [ProcAction("Open bookmark", ["xdg-open", url])])
+            for name, exp, text, url in bookmarks if (exp.startswith(query)) ]
