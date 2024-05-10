b34a8ad7f6e11cf173800a37e03e2055408baeff
Pablo Herrero <pablodherrero@gmail.com>
2024-05-10 00:01:22 -0300

    Update for Albert Plugin Interface v2.0

--- __init__.py
+++ __init__.py
@@ -11,53 +11,88 @@ Synopsis: <trigger> [delay|throw] <query>"""
 
 from albert import *
 from pathlib import Path
-from unidecode import unidecode
 from urllib.parse import urlparse
-from PyQt5 import QtCore
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from threading import Thread
 import os.path
 
-__title__ = "Mate Bookmarks"
-__version__ = "0.0.1"
-__authors__ = "Pablo Herrero"
-__py_deps__ = ['unidecode', 'PyQt5']
+md_iid = "2.0"
+md_version = "0.2"
+md_name = "Mate Bookmarks"
+md_description = "Launch Bookmarks from Caja"
+md_lib_dependencies = ['PyQt5', 'ipdb', 'watchdog']
+md_license = "MIT"
+md_url = "https://github.com/pabloh/mate_bookmarks"
+md_authors = "@pabloh"
 
-iconPath = iconLookup("folder")
-xdg_config_dir = os.environ.get('XDG_CONFIG_HOME') or os.path.join(Path.home(), '.config')
-bookmarks_file = os.path.join(xdg_config_dir, 'gtk-3.0', 'bookmarks')
+class Plugin(PluginInstance, GlobalQueryHandler):
+    xdg_icon = "xdg:folder"
+    xdg_config_dir = os.environ.get('XDG_CONFIG_HOME') or os.path.join(Path.home(), '.config')
+    bookmarks_file = os.path.join(xdg_config_dir, 'gtk-3.0', 'bookmarks')
 
-def get_bookmark(url):
-    name = url.path.rstrip(os.sep).split(os.sep)[-1]
-    return (name, unidecode(name.lower()), f'Open {url.path if url.scheme == "file" else url.geturl()}', url.geturl())
+    def finalize(self):
+        # info('Finalize ' + md_id)
+        self.observer.stop_watching()
 
-def update_bookmarks():
-    global bookmarks
+    def __init__(self):
+        # info('Initialize ' + md_id)
+        GlobalQueryHandler.__init__(self, md_id, md_name, md_description, defaultTrigger=md_id)
+        PluginInstance.__init__(self, extensions=[self])
 
-    if os.path.isfile(bookmarks_file):
-        with open(bookmarks_file, 'r') as file:
-            urls = (urlparse(l) for l in file.read().splitlines())
-            bookmarks = [get_bookmark(u) for u in urls if u.scheme != 'file' or u.path != Path.home()]
-    else:
-        bookmarks = []
+        self.update_bookmarks()
+        self.observer = BookmarkWatcher.start_watching_on_bg(self)
 
-def set_watcher():
-    global fs_watcher
+    def handleGlobalQuery(self, query):
+        exp_query = query.string.lower()
+        return [
+            RankItem(
+                item=StandardItem(
+                    id=exp,
+                    text=text,
+                    subtext=subtext,
+                    iconUrls=[self.xdg_icon],
+                    actions=[Action("open", "Open bookmark", lambda: runDetachedProcess(["xdg-open", url]))]),
+                score=1.0)
+            for text, exp, subtext, url in self.bookmarks if (exp.startswith(exp_query))
+        ]
 
-    fs_watcher = QtCore.QFileSystemWatcher([bookmarks_file])
-    fs_watcher.fileChanged.connect(file_changed)
+    def get_bookmark(self, url):
+        dirname = url.path.rstrip(os.sep).split(os.sep)[-1]
+        return (dirname, dirname.lower(), f'Open {url.path if url.scheme == "file" else url.geturl()}', url.geturl())
 
-@QtCore.pyqtSlot(str)
-def file_changed(_):
-    initialize()
+    def update_bookmarks(self):
+        # info('Updating bookmarks')
+        if os.path.isfile(self.bookmarks_file):
+            with open(self.bookmarks_file, 'r') as file:
+                urls = (urlparse(l) for l in file.read().splitlines())
+                self.bookmarks = [self.get_bookmark(u) for u in urls if u.scheme != 'file' or u.path != Path.home()]
+        else:
+            self.bookmarks = []
 
-def initialize():
-    update_bookmarks()
-    set_watcher()
+class BookmarkWatcher(FileSystemEventHandler):
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.bookmarks_file = plugin.bookmarks_file
+        self.observer = Observer()
+        self.observer.schedule(self, os.path.dirname(self.bookmarks_file), recursive=False)
 
-def handleQuery(query):
-    if not query.string:
-        return
+    @classmethod
+    def start_watching_on_bg(cls, plugin):
+        bw = cls(plugin)
+        bw.start_watching()
+        return bw
 
-    query = unidecode(query.string.lower())
+    def start_watching(self):
+        self.thread = Thread(target=self.observer.start)
+        self.thread.start()
 
-    return [ Item(__title__, iconPath, name, text, [ProcAction("Open bookmark", ["xdg-open", url])])
-            for name, exp, text, url in bookmarks if (exp.startswith(query)) ]
+    def stop_watching(self):
+        self.observer.stop()
+        self.thread.join()
+
+    def on_deleted(self, event): self.on_modified(event)
+
+    def on_modified(self, event):
+        if event.src_path.endswith(self.bookmarks_file):
+            self.plugin.update_bookmarks()
